adf77c374eb91850a640969d023ed8541baee261
unknown <recruiterhrpartner4@gmail.com>
2022-05-20 17:47:29 +0300

    второе задание со звездочкой

--- Lesson_1/script.sh
+++ Lesson_1/script.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+#Написать скрипт который выполнит автоматически пункты
+# - Создать папку
 mkdir script_1
+# - Зайти в папку
 cd script_1
+# - Создать 3 папки
 mkdir script_2 script_3 script_4
+# - Зайти в любоую папку
 cd script_2
+# - Создать 5 файлов (3 txt, 2 json)
 touch script_1.txt script_2.txt script_3.txt script_4.json script_5.json
+# - Создать 3 папки
 mkdir script_5 script_6 script_7
+# - Вывести список содержимого папки
 ls -la
+# - Переместить любые 2 файла, которые вы создали, в любую другую папку.
 mv script_1.txt script_5/script_1.txt
 mv script_4.json script_5/script_4.json
 
+
+# Чтобы сделать файл исполняемым: chmod +x ./script.sh
+# Запускаем скрипт: ./script.sh
